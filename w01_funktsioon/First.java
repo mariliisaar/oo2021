720bd9df86c6cb806844913a9faebffd951b8a89
Marilii Saar <ly@marilii.com>
2021-02-05 14:19:24 +0200

    Added First

---
+++ w01_funktsioon/First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Test one");
+        System.out.println("Test two");
+        System.out.print("\n");
+
+        String hello = "Hello world!";
+        System.out.print(hello);
+    }
+}
