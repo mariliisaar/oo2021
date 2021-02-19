5bc297a37ea45bc4b3100da6832fd7e35c6a0c64
Marilii Saar <mariliisaar@users.noreply.github.com>
2021-02-19 08:10:14 +0200

    HashMap

--- w02_klass/Maps.java
+++ w02_klass/Maps.java
@@ -1,9 +1,30 @@
+import java.util.HashMap;
+
 public class Maps {
     public static void main(String[] args) {
-        
+        hashMap();
     }
 
     public static void hashMap() {
-        
+        HashMap<String, String> capitals = new HashMap<String, String>();
+
+        capitals.put("Estonia", "Tallinn");
+        capitals.put("France", "Paris");
+        capitals.put("Germany", "Berlin");
+        capitals.put("Latvia", "Riga");
+        capitals.put("Lithuania", "Vilnius");
+        capitals.put("Finland", "Helsinki");
+
+        System.out.println(capitals);
+        System.out.println(capitals.get("Estonia"));
+        System.out.println(capitals.size());
+
+        for (String i : capitals.keySet()) {
+            System.out.println(i);
+        }
+
+        for (String i : capitals.values()) {
+            System.out.println(i);
+        }
     }
 }
